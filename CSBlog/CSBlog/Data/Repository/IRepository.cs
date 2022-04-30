a35df8ff16f7667fe3f01c9015a146b7459ed1fc
skripkalisa <skripkalisa.msk@ya.ru>
2022-04-30 16:29:45 +0300

    Updates to repo structure

---
+++ CSBlog/CSBlog/Data/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+using CSBlog.Models.Blog;
+
+namespace CSBlog.Models.Repository;
+
+public interface IRepository<T> where T : class
+{
+  IEnumerable<T> GetAll();
+  T Get(int id);
+  void Create(T item);
+  void Update(T item);
+  void Delete(T item);
+}
